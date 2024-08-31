8ab9c7b642f8335b68d003a79d1fc69ef6079cd4
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-09-01 06:26:15 +0700

    Files upload

---
+++ lib/utils.ts
@@ -0,0 +1,27 @@
+import { type Metadata } from 'next';
+
+export const createMetadata = (
+  title: string,
+  description: string,
+  image: string,
+  url: URL
+): Metadata => ({
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: [{ url: image }],
+    url,
+    siteName: title
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: [image],
+    creator: '@piaseckijulian'
+  },
+  icons: ['/favicon.ico'],
+  metadataBase: url
+});
